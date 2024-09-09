e47b21bcc741460fa150a55f767e1372b2220719
subhankar-creator <130303262+subhankar-creator@users.noreply.github.com>
2024-09-09 21:01:32 +0530

    Add files via upload

---
+++ 1D Array Coding Practice/P_1.java
@@ -0,0 +1,19 @@
+package arrays.practice_9;
+
+public class P_1 {
+
+	public static void main(String[] args) {
+		
+		int arr [] = new int [3];   // initialising the array 
+		
+		arr [0]=14;
+		arr [1]=12;
+		arr [2]=10;
+		
+		System.out.println("arr [0] is :"+ arr [0] );
+		System.out.println("arr [1] is :"+ arr [1] );
+		System.out.println("arr [2] is :"+ arr [2] );
+
+	}
+
+}
